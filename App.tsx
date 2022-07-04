970356c5172e8bec7ecc9c7c05266d5ab7769a78
PaulinhoMachadooo <paulinhomachadoo20@gmail.com>
2022-07-04 20:38:50 -0300

    Estrutura da Calculadora

--- App.tsx
+++ App.tsx
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+  class Botao extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    let c = 1;
+    if(props.c) {
+        c = parseInt (props.c);
+    }
+
+    let bg = '#E0E0E0';
+    if(props.bg) {
+        bg = props.bg;
+    }
+
+
+    this.styles = StyleSheet.create({
+      area:{
+          flex:c,
+          justifyContent:'center',
+          alignItems:'center',
+          borderWidth:1,
+          borderColor:'#999999',
+          backgroundColor: bg,
+      },
+      text:{
+        fontSize:18,
+      }
+
+    });
+  }  
+
+  render() {
+    return (
+        <TouchableOpacity style={this.styles.area}>
+            <Text style={this.styles.text}>{this.props.n}</Text>
+        </TouchableOpacity>
+    );
+  }  
+  
+}
+
+export default class ProjetoAPPCalculadora extends Component {
+
+  render() {
+    return (
+      <View style={styles.body}>
+          <View style={styles.linha}>
+            <Text></Text>
+          </View>
+          <View style={styles.linha}>
+              <Botao c="3" n="C" bg="#CCC" />
+              <Botao n="/" bg="#FD9536" />
+          </View>
+          <View style={styles.linha}>
+              <Botao n="7" />
+              <Botao n="8" />
+              <Botao n="9" />
+              <Botao n="*" bg="#FD9536" />
+          </View>
+          <View style={styles.linha}>
+              <Botao n="4" />
+              <Botao n="5" />
+              <Botao n="6" />
+              <Botao n="-" bg="#FD9536" />
+          </View>
+          <View style={styles.linha}>
+              <Botao n="1" />
+              <Botao n="2" />
+              <Botao n="3" />
+              <Botao n="+" bg="#FD9536"/>
+          </View>
+          <View style={styles.linha}>
+              <Botao c="2" n="0" />
+              <Botao n="." />
+              <Botao n="=" bg="#FD9536" />
+          </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body:{
+    paddingTop:20,
+    flex:1
   },
+  linha:{
+    flex:1,
+    flexDirection:'row',
+  }
 });
